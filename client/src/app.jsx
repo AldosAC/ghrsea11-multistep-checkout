fa407687026aca895f0c96d2386df1d5862fc5f0
Joel Carpenter <aldosac@gmail.com>
2020-07-29 12:25:30 -0700

    Initial setup of app.jsx and react

---
+++ client/src/app.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "../../public/react.development.js";
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      view: ""
+    }
+  }
+
+  render () {
+    
+  }
+};
